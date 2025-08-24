47c40fa8d76f140bf4a90c42a5e6b4333d12d2ae
Kimera Royal <kim@kimera.world>
2025-08-24 12:46:36 +0100

    Moved carousel script to new entry point

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,32 +1,5 @@
 const build = require("./config/esbuild.defaults.js")
 
-// You can customize this as you wish, perhaps to add new esbuild plugins.
-//
-// ```
-// const path = require("path")
-// const esbuildCopy = require('esbuild-plugin-copy').default
-// const esbuildOptions = {
-//   plugins: [
-//     esbuildCopy({
-//       assets: {
-//         from: [path.resolve(__dirname, 'node_modules/somepackage/files/*')],
-//         to: [path.resolve(__dirname, 'output/_bridgetown/somepackage/files')],
-//       },
-//       verbose: false
-//     }),
-//   ]
-// }
-// ```
-//
-// You can also support custom base_path deployments via changing `publicPath`.
-//
-// ```
-// const esbuildOptions = {
-//   publicPath: "/my_subfolder/_bridgetown/static",
-//   ...
-// }
-// ```
-
 /**
  * @typedef { import("esbuild").BuildOptions } BuildOptions
  * @type {BuildOptions}
@@ -37,7 +10,11 @@ const esbuildOptions = {
   ],
   globOptions: {
     excludeFilter: /\.(dsd|lit)\.css$/
-  }
+  },
+  entryPoints: [
+    "./frontend/javascript/index.js",
+    "./frontend/javascript/carousel.js",
+  ]
 }
 
 build(esbuildOptions)
--- frontend/javascript/carousel.js
+++ frontend/javascript/carousel.js
@@ -1,5 +1,6 @@
 (function loadCarousels() {
     var carousel = document.querySelector('.carousel');
+    console.log(carousel);
     var flkty = new Flickity(carousel, {
         draggable: false,
         wrapAround: true,
